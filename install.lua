dfcbdee453e6c5f7ad91a644cd3546deccef3230
Eric Sexton <foopis23@gmail.com>
2025-05-04 16:55:33 -0400

    added install script

---
+++ install.lua
@@ -0,0 +1,55 @@
+function trim(s)
+	return (s:gsub("^%s*(.-)%s*$", "%1"))
+ end
+
+
+print("Most packages depend on unix-like paths, would you like to install unix-like paths? (Y/n)")
+term.setTextColor(colors.lightGray)
+print("This will create a startup file to add /bin to path and start you in your /home directory.")
+term.setTextColor(colors.white)
+
+local answer = read()
+answer = trim(answer)
+answer = string.lower(answer)
+
+if answer=="" then
+  answer = "y"
+end
+
+if answer=="y" or answer=="yes" then
+	if not fs.exists("/bin") then
+		fs.makeDir("/bin")
+	end
+
+	if not fs.exists("/lib") then
+		fs.makeDir("/lib")
+	end
+
+	if not fs.exists("/home") then
+		fs.makeDir("/home")
+	end
+
+	if not fs.exists("/etc") then
+		fs.makeDir("/etc")
+	end
+
+	if not fs.exists("/usr") then
+		fs.makeDir("/usr")
+	end
+
+	if not fs.exists("/var") then
+		fs.makeDir("/var")
+	end
+
+	if not fs.exists("/startup") then
+		fs.makeDir("/startup")
+	end
+
+	local startup_file = fs.open("/startup/50_unix_paths.lua", "w")
+	startup_file.write('shell.setPath("/bin"..shell.path())\nshell.setDir("/home")\n');
+	startup_file.close()
+
+	shell.run("wget https://raw.githubusercontent.com/foopis23/cc-pack/refs/heads/main/bin/ccp.lua /bin/ccp.lua")
+else
+	shell.run("wget https://raw.githubusercontent.com/foopis23/cc-pack/refs/heads/main/bin/ccp.lua ccp.lua")
+end
